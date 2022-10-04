a01d7b646a391d1a66671cb067f298149df5eee8
kam071 <kam071@bucknell.edu>
2022-10-04 09:19:05 -0400

    Lab 6, problem 2 completed

---
+++ Labs/Lab6/problem2.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <features.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <semaphore.h>
+
+
+void *Philosopher(void *id);
+void napping(int t);
+pthread_mutex_t chopsticks[5];
+
+
+int main() {
+  pthread_t threads[5];
+  for (int i = 0; i < 5; i++) {
+	pthread_mutex_init(&chopsticks[i], NULL);
+  }
+  for (int i = 0; i < 5; i++) {
+	pthread_create(&threads[i], NULL, Philosopher, (void *) i);
+  }
+  pthread_exit(NULL);
+}
+
+void *Philosopher(void *id) {
+  long long n_id = (long long) id;
+  while (1) {
+	printf("Philosopher %lld is thinking.\n", n_id);
+	// This ensures that the philosophers will pick up the left chopstick in ascending order
+	napping(1 + n_id); 
+	printf("Philosopher %lld is hungry.\n", n_id);
+	pthread_mutex_lock(&chopsticks[n_id]);
+	printf("Philosopher %lld is picking up chopstick %lld\n", n_id, n_id);
+	// The philosophers will wait a long time before picking up another chopstick. This makes it so all of the left chopsticks are picked up before any right ones, creating a deadlock. 
+	napping(100);
+	pthread_mutex_lock(&chopsticks[(n_id + 1) % 5]);
+	printf("Philosopher %lld is picking up chopstick %lld\n", n_id, (n_id + 1) % 5);
+	printf("Philosopher %lld is starting to eat.\n", n_id);
+	napping(1);
+	printf("Philosopher %lld is done eating.\n", n_id);
+	pthread_mutex_unlock(&chopsticks[(n_id + 1) % 5]);
+	printf("Philosopher %lld is putting down chopstick %lld\n", n_id, (n_id + 1) % 5);
+	pthread_mutex_unlock(&chopsticks[n_id]);
+	printf("Philosopher %lld is putting down chopstick %lld\n", n_id, n_id);
+	fflush(stdout);
+  }
+}
+
+void napping(int t) {
+  //unsigned int seed = rand();
+  unsigned int seed = t;
+  int r = rand_r(&seed) % (t * 1000);
+  int seconds = r / 1000;
+  int milliseconds = r % 1000;
+  sleep(seconds);
+  usleep(milliseconds * 1000);
+}
+
