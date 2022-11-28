a28c691d2645c3eaa9e014cfafb31e2fd41fb98d
kam071 <kam071@bucknell.edu>
2022-11-28 18:16:52 -0500

    Lab 9.3 completed

--- Labs/Lab9/ishell.c
+++ Labs/Lab9/ishell.c
@@ -5,42 +5,55 @@
 #include "wrapper.h"
 #include "wrapper.c"
 
+void execute(char **com) {  
+  int status;
+  int returnStat;
+  int pid = Fork();
+  
+  if (pid == 0) {
+	//child process
+	returnStat = execvp(*com, com);
+	if (returnStat < 0) {
+	  printf("[ishell: program terminated abnormally][%d]\n", returnStat);
+	  exit(1);
+	}
+  }
+  else {
+	//parent process
+	while (wait(&status) != pid);
+	if (status == 0) {
+	  printf("[ishell: program terminated successfully]\n");
+	}
+  } 
+}
+
 void split(char **com, char *prompt) {
   char *token = NULL;
   int arg = 0;
   while (token = strtok_r(prompt, " ", &prompt)) {
-	com[arg] = token;
-	arg += 1;
+	if (token[strlen(token) - 1] == ';') {
+	  token[strlen(token) - 1] = 0;
+	  com[arg] = token;
+	  arg++;
+	  execute(com);
+	  arg = 0;
+	}
+	else {
+	  com[arg] = token;
+	  arg++;
+	}
   }
+  com[arg] = NULL;
 }
 
 int main(int argc, char *argv[]) {
   char *com[256];
-  int returnStat;
-  int status;
   
   while (1) {
 	char *prompt = readline("ishell> ");
-	
-	
+
 	split(com, prompt);
-	
-	int pid = Fork();
-	
-	if (pid > 0) {
-	  //parent process
-	  wait(&status);
-	  if (status == 0) {
-		printf("[ishell: program terminated successfully]\n");
-	  }
-	}
-	else {
-	  //child process
- 	  returnStat = execvp(*com, com);
-	  if (returnStat < 0) {
-		printf("[ishell: program terminated abnormally][%d]\n", returnStat);
-		exit(1);
-	  }
-	}
+
+	execute(com);
   }
 }
