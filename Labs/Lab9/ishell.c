2eed96c852d07a732763a28f321950382c075d97
kam071 <kam071@bucknell.edu>
2022-11-28 17:22:36 -0500

    Lab 9.2 completed

--- Labs/Lab9/ishell.c
+++ Labs/Lab9/ishell.c
@@ -16,21 +16,31 @@ void split(char **com, char *prompt) {
 
 int main(int argc, char *argv[]) {
   char *com[256];
+  int returnStat;
+  int status;
   
   while (1) {
 	char *prompt = readline("ishell> ");
-
+	
+	
 	split(com, prompt);
 	
 	int pid = Fork();
 	
 	if (pid > 0) {
 	  //parent process
-	  wait(NULL);
+	  wait(&status);
+	  if (status == 0) {
+		printf("[ishell: program terminated successfully]\n");
+	  }
 	}
 	else {
 	  //child process
-	  execvp(*com, com);
+ 	  returnStat = execvp(*com, com);
+	  if (returnStat < 0) {
+		printf("[ishell: program terminated abnormally][%d]\n", returnStat);
+		exit(1);
+	  }
 	}
   }
 }
